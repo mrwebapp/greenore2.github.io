95584be843ce8b47d7c2288e5055be1557d602ab
greenore <tim.hagmann@gmail.com>
2014-04-29 14:24:18 +0200

    css and js upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+// First function
+$(document).ready(function () {
+    "use strict";
+    $('#title').after('<p id="txt">My own text</p>');
+});
